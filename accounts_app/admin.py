fbc29d30c560be5b0aca4fb57e8f49b8f3c5e915
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-23 00:46:38 +0430

    add user model to accounts_app app

--- accounts_app/admin.py
+++ accounts_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Users
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(Users, UserAdmin)
+
+UserAdmin.fieldsets[1][1]['fields'] = ("first_name", "last_name", "email", "address", "phone", "receiver_name", "post_id")
--- accounts_app/models.py
+++ accounts_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Users(AbstractUser):
+    email = models.EmailField(unique=True)
+    receiver_name = models.CharField(verbose_name='Reciever Name', max_length=50)
+    phone = models.CharField(max_length=11)
+    address = models.TextField()
+    post_id = models.CharField(max_length=10)

008964e73275a7d7a488615fbe230eab3fa1d30a
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-23 00:56:44 +0430

    add null=True, blank=True to Users fields except email

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Users(AbstractUser):
     email = models.EmailField(unique=True)
-    receiver_name = models.CharField(verbose_name='Reciever Name', max_length=50)
-    phone = models.CharField(max_length=11)
-    address = models.TextField()
-    post_id = models.CharField(max_length=10)
+    receiver_name = models.CharField(verbose_name='Reciever Name', null=True, blank=True, max_length=50)
+    phone = models.CharField(max_length=11, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    post_id = models.CharField(max_length=10, null=True, blank=True)

ceaf63f6d6b3ada0f1b25cfd112923ffadb5c108
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-23 13:32:28 +0430

    add register form

---
+++ accounts_app/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreatForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password1', 'password2')

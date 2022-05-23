91c8472b49415cfd3d960778239f584326f2507c
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-23 10:56:13 +0430

    add home page

--- digikala/urls.py
+++ digikala/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts_app.urls')),
 ]

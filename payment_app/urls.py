8d777be03eb50f39c2be4a24717d23f14e2afef1
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-26 00:41:05 +0430

    add shopping payment, complete payment and no complete payment to paymeny app

--- payment_app/urls.py
+++ payment_app/urls.py
@@ -1,7 +1,13 @@
+from re import template
 from django.urls import path
+from django.views.generic import TemplateView
+from .views import ShoppingPaymentView, payment_compelete_view, payment_no_compelete_view
+
 
 app_name = 'payment_app'
 
 urlpatterns = [
-    
+    path('shopping/payment/', ShoppingPaymentView.as_view(), name='shopping-payment'),
+    path('shopping/payment/compelete/', payment_compelete_view, name='shopping-compelete'),
+    path('shopping/payment/no/compelete/', payment_no_compelete_view, name='shopping-no-compelete'),
 ]
--- payment_app/views.py
+++ payment_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from store_app.models import Cart
 
-# Create your views here.
+
+class ShoppingPaymentView(ListView):
+    model = Cart
+    template_name = 'payment_app/shopping-payment.html'
+    context_object_name = 'carts'
+
+    def get_queryset(self):
+        return Cart.objects.filter(user__username=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_price'] = sum([int(c.product.price) for c in Cart.objects.filter(user__username='mamali')])
+        context['total_count'] = Cart.objects.filter(user__username=self.request.user).count()
+        return context
+
+
+def payment_compelete_view(request):
+    total_price = sum([int(c.product.price) for c in Cart.objects.filter(user__username='mamali')])
+    context = {'total_price': total_price}
+    return render(request, 'payment_app/shopping-complete-buy.html', context)
+
+
+def payment_no_compelete_view(request):
+    total_price = sum([int(c.product.price) for c in Cart.objects.filter(user__username='mamali')])
+    context = {'total_price': total_price}
+    return render(request, 'payment_app/shopping-no-complete-buy.html', context)

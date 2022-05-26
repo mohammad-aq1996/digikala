fecf0d9981d86f00ae25574adb9fa9795fb22c0f
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-26 17:00:34 +0430

    add slug link to base temp: iphone and xiaomi

--- store_app/urls.py
+++ store_app/urls.py
@@ -5,7 +5,7 @@ app_name = 'store_app'
 
 urlpatterns = [
     path('mobiles/', MobileListView.as_view(), name='mobile-list'),
-    path('mobile/<slug:brand>/', MobileBrandListView.as_view(), name='mobile-iphone-list'),
+    path('mobile/<slug:brand>/', MobileBrandListView.as_view(), name='mobile-brand-list'),
     path('mobile/<int:pk>/', MobileDetailView.as_view(), name='mobile-detail'),
     path('cart/', CartListView.as_view(), name='cart'),
     path('d/<int:pk>/', remove_from_cart, name='remove_from_cart'),

284621c3a1a3e937380c2614f69dfb2c7cdb3de5
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-27 20:31:15 +0430

    add jalali date to project

--- digikala/settings.py
+++ digikala/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'django.contrib.humanize',
     'ckeditor',
+    'jalali_date',
 
     'accounts_app.apps.AccountsAppConfig',
     'store_app.apps.StoreAppConfig',
@@ -214,4 +215,30 @@ CKEDITOR_CONFIGS = {
             'elementspath'
         ]),
     }
+}
+
+
+# default settings
+JALALI_DATE_DEFAULTS = {
+   'Strftime': {
+        'date': '%y/%m/%d',
+        'datetime': '%H:%M:%S _ %y/%m/%d',
+    },
+    'Static': {
+        'js': [
+            # loading datepicker
+            'admin/js/django_jalali.min.js',
+            # OR
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            # 'admin/js/main.js',
+        ],
+        'css': {
+            'all': [
+                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+            ]
+        }
+    },
 }

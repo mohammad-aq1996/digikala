43173e48c2e8d5a83dda4fda41b9328147e2a3eb
Mohammad <mohammad.aq1996@outlook.com>
2022-07-26 12:08:21 +0430

    modify store_app admin panel

--- store_app/admin.py
+++ store_app/admin.py
@@ -2,8 +2,30 @@ from django.contrib import admin
 from .models import Brand, Category, Product, Comment, Cart
 
 
-admin.site.register(Brand)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Comment)
-admin.site.register(Cart)
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ['title']}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ['title']}
+
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+    list_display = ['user', 'status']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['persian_title', 'price', 'category', 'count']
+    inlines = [CommentAdmin]
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'quantity']
+
+

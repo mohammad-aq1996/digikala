d1450359d6f1cf25759e3ff7eafadb524bd660e9
Mohammad <mohammad.aq1996@outlook.com>
2022-07-27 21:10:56 +0430

    create .env file and add SECRET_KEY and DEBUG to it

--- digikala/settings.py
+++ digikala/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6!$a8#%#k_*!q7^^8vr3_@4e#-u9!r1pnl*h9$#$ln2(w-abg'
+SECRET_KEY = os.environ.get('.env', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('.env', 'DEBUG')
 
 ALLOWED_HOSTS = []
 

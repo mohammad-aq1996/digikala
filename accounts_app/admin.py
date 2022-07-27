2b1cec8060f0f7cd2c5967da06cce12c590be229
Mohammad <mohammad.aq1996@outlook.com>
2022-07-27 18:35:33 +0430

    delete unneccessary line in accounts_app.admin

--- accounts_app/admin.py
+++ accounts_app/admin.py
@@ -1,14 +1,3 @@
-# from django.contrib import admin
-# from .models import Users
-# from django.contrib.auth.admin import UserAdmin
-
-# admin.site.register(Users, UserAdmin)
-
-# UserAdmin.fieldsets[1][1]['fields'] = ("first_name", "last_name", "email", "address", 
-#                                         "phone", "receiver_name", "post_id", 'national_code', 
-#                                         'newsletter','card_number')
-
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext as _

daccca6f96a92252325c128d51d51a38d857c6db
Mohammad <mohammad.aq1996@outlook.com>
2022-07-27 18:40:50 +0430

    modify store_app/context_proccessors.py: delete useless test func and rename  function

--- store_app/context_processors.py
+++ store_app/context_processors.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
 from .models import Cart
 
 
 
-def b(request):
+def cart_context(request):
     context = {
         
         'carts':Cart.objects.filter(user__email=request.user),
         'total_price':sum([int(c.product.price)*int(c.quantity) for c in Cart.objects.filter(user__email=request.user)])
     }
     return context
-
-def c(request):
-        return {'h':10}

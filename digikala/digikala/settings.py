0ad95c43817350fbda064646751eabf76a48901f
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-08-01 18:11:45 +0430

    allow all hosts

--- digikala/digikala/settings.py
+++ digikala/digikala/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('.env', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('.env', 'DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

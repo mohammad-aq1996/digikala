f45ef2efc909bd91d72d22a5a9be10123d41035e
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-08-01 18:58:51 +0430

    add PostgreSQL settings

--- digikala/digikala/settings.py
+++ digikala/digikala/settings.py
@@ -90,13 +90,22 @@ WSGI_APPLICATION = 'digikala.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        # 'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
